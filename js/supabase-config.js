ce9dd5dcee9656bf8895ea8e4bac12c6f41fd17c
Top-Desenvolvimento <topesteticabucal.gerencia2@gmail.com>
2026-04-07 10:50:54 -0300

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -2,3 +2,4 @@ const SUPABASE_URL = "https://dgmqwcbjbluoplczfflo.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRnbXF3Y2JqYmx1b3BsY3pmZmxvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUxMjk4MzYsImV4cCI6MjA5MDcwNTgzNn0.qCBdP9nabXepmPxwFnUgPrhMofpR5vmPWAgdOJmHeEs";
 
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+window.supabaseClient = supabaseClient;
